fa8efcb7eaa8e6e00cd2cf48d46d96ec6b0b0809
Choiheeyoung <choiheeyoung10@gmail.com>
2023-09-16 19:02:42 +0900

    [codeup] 1054번

---
+++ codeup 1054.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+    if (num1 == 1 && num2 == 1)
+    {
+        printf("1");
+    }
+    else
+    {
+        printf("0");
+    }
+    return 0;
+}
