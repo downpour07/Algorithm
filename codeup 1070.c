82727e505cffb53b5b02b5a7d89ce85ccbeb5f3a
Choiheeyoung <choiheeyoung10@gmail.com>
2023-10-04 08:09:42 +0900

    [codeup] 1070번

---
+++ codeup 1070.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    scanf("%d", &x);
+    switch (x)
+    {
+    case 12:
+    case 1:
+    case 2:
+        printf("winter\n");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        printf("spring\n");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        printf("summer\n");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        printf("fall\n");
+        break;
+    }
+    return 0;
+}
