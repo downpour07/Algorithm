a959faf9b44cee20857429127d09d063b63c300e
downpour07 <choiheeyoung10@gmail.com>
2023-10-27 23:36:41 +0900

    [codeup] 1094번

---
+++ codeup 1094.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    int a[10000] = { 0 };
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) {
+        scanf("%d", &a[i]);
+    }
+    for (i = n; i >= 1; i--) {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
