756fa2c2bd43d31904467ccb275c4ac41658db62
downpour07 <choiheeyoung10@gmail.com>
2023-10-29 10:57:21 +0900

    [codeup] 1096번

---
+++ codeup 1096.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[20][20] = { 0 };
+    int n, i, j, x, y;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        scanf("%d %d", &x, &y);
+        a[x][y] = 1;
+    }
+    for (i = 1; i <= 19; i++)
+    {
+        for (j = 1; j <= 19; j++)
+            printf("%d ", a[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
