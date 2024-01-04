7fe9efab9d525a532db6e89c0aa80bd102e62e59
최희영 <127270678+downpour07@users.noreply.github.com>
2024-01-04 09:39:25 +0900

    [level unrated] Title: 공배수, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181936. 공배수/공배수.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int number, int n, int m) {
+    int answer = 0;
+    if(number%n==0)
+        if(number % m ==0)
+            answer = 1;
+    return answer;
+}
