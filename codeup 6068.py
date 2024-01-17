b82d5456c50b09326c9cdaa713a6cc56b4f23a68
downpour07 <choiheeyoung10@gmail.com>
2024-01-17 16:07:22 +0900

    [codeup] 6068번

---
+++ codeup 6068.py
@@ -0,0 +1,11 @@
+a = int(input())
+if(a>=90):
+    print("A")
+else:
+    if(a>=70):
+        print("B")
+    else:
+        if(a>=40):
+            print("C")
+        else:
+            print("D")
