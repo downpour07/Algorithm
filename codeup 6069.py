43a8fee6c7a676729c0bcec2f5e7cd7fed164b67
downpour07 <choiheeyoung10@gmail.com>
2024-01-17 16:07:30 +0900

    [codeup] 6069번

---
+++ codeup 6069.py
@@ -0,0 +1,14 @@
+a = input()
+if(a == 'A'):
+    print("best!!!")
+else:
+    if(a=='B'):
+        print("good!!")
+    else:
+        if(a=='C'):
+            print("run!")
+        else:
+            if(a=='D'):
+                print("slowly~")
+            else:
+                print("what?")
