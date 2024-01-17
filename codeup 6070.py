42188adeb4486a64dca8551a4323884f60ce5678
downpour07 <choiheeyoung10@gmail.com>
2024-01-17 16:07:43 +0900

    [codeup] 6070번

---
+++ codeup 6070.py
@@ -0,0 +1,11 @@
+a = int(input())
+if(a>=3 and a<=5):
+    print("spring")
+else:
+    if(a>=6 and a<=8):
+        print("summer")
+    else:
+        if(a>=9 and a<=11):
+            print("fall")
+        else:
+            print("winter")
