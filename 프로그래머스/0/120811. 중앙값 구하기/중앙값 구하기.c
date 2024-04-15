5388086da85eecface43b4a0f1b55b718cb49a32
최희영 <127270678+downpour07@users.noreply.github.com>
2024-04-15 10:12:31 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// array_len은 배열 array의 길이입니다.
+int solution(int array[], size_t array_len) {
+    int answer = 0;
+    
+    for (int i = 0; i < array_len; i++) {
+        for (int j = 0; j < (array_len - 1) - i; j++) {
+            if (array[j] > array[j + 1]) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    
+    answer = array[array_len / 2];
+    return answer;
+}
