55b9134e482dc9f3f8eff468d436e0ae64f2695b
최희영 <127270678+downpour07@users.noreply.github.com>
2024-04-15 10:58:42 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// num_list_len은 배열 num_list의 길이입니다.
+int* solution(int num_list[], size_t num_list_len) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(1000*sizeof(int*));
+    int a = 0;
+    for(int i = num_list_len - 1; i>=0; i--) {
+        answer[a++] = num_list[i];
+    }
+    return answer;
+}
