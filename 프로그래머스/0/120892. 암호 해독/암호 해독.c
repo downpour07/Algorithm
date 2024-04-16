4fc889ab1e64d6c8bf6753cdffd6a13837095750
최희영 <127270678+downpour07@users.noreply.github.com>
2024-04-16 23:12:57 +0900

    [level 0] Title: 암호 해독, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* cipher, int code) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* answer = (char*)malloc(1000*sizeof(char*));
+    int a = 0;
+    for(int i = code-1; i<strlen(cipher); i+=code) {
+        answer[a++] = cipher[i];
+    }
+    return answer;
+}
