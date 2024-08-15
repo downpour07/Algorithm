6b517d532bd6389698f462b8c5c0cd4c5360610c
최희영 <127270678+downpour07@users.noreply.github.com>
2024-08-15 17:22:47 +0900

    [level 0] Title: k의 개수, Time: 6.35 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+int solution(int i, int j, int k) {
+    int answer = 0;
+    char ck = k + 48;
+    char ci[3];
+
+    for(; i<=j; i++)
+    {
+        sprintf(ci, "%d", i);
+        for(int o = 0; o<strlen(ci); o++){
+            if(ci[o] == ck){
+                answer++;
+            }
+        }
+    }
+    
+    return answer;
+}
