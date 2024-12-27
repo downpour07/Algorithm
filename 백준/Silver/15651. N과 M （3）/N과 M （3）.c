79f09a891922e9919caf8042d88fda5485b269aa
최희영 <127270678+downpour07@users.noreply.github.com>
2024-12-27 16:03:42 +0900

    [Silver III] Title: N과 M (3), Time: 440 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+int n, m;
+int arr[7];
+void nm(int cnt);
+ 
+int main() {
+	scanf("%d %d", &n, &m);
+	nm(0);
+	return 0;
+}
+ 
+void nm(int count) {
+	if (count == m) {
+		for (int i = 0; i < m; i++) {
+			printf("%d ", arr[i]);
+        }
+		printf("\n");
+	}
+	else {
+		for (int i = 1; i <= n; i++) {
+			arr[count] = i;
+			nm(count + 1);
+		}
+	}
+}
